a12e522823b2184f0cd39f292d748801296d2086
jrao2003 <robles.otero.jose.angel@gmail.com>
2025-01-16 23:45:40 -0600

    ya se visualiza la imagen y el pdf

--- lectura/views.py
+++ lectura/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+
+
+
 
 
 # Create your views here.
@@ -8,8 +14,30 @@ def bienvenida(request):
 
 def cargar_archivo(request):
     #aqui va el back de cargar archivo
-    return render(request, 'leer.html')
+    if request.method == 'POST' and request.FILES['file']:
+        archivo_pdf = request.FILES['file']
+        # Guardar el archivo en la carpeta local
+        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+        filename = fs.save(archivo_pdf.name, archivo_pdf)
+        file_url = os.path.join(settings.MEDIA_URL, filename)
+        print(file_url)
+        return render(request, 'leer.html', {'file_url': file_url})
+    return render(request, 'index.html')
 
 def subir_foto(request):
     #aqui va el back de subir foto
-    return render(request, 'foto.html')
+    if request.method == 'POST' and request.FILES.get('photo'):
+        photo = request.FILES['photo']
+        save_path = os.path.join(settings.MEDIA_ROOT, 'fotos', photo.name)
+
+        # Guarda la foto en la carpeta media/fotos
+        with open(save_path, 'wb+') as destination:
+            for chunk in photo.chunks():
+                destination.write(chunk)
+
+        # Genera la URL para acceder al archivo
+        photo_url = os.path.join(settings.MEDIA_URL, 'fotos', photo.name)
+        return render(request, 'foto.html', {'photo_url': photo_url})
+
+    photo_url = os.path.join(settings.MEDIA_URL, 'fotos', 'captured_photo.png')
+    return render(request, 'foto.html', {'photo_url': photo_url})
